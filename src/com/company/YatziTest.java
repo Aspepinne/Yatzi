e0cbce33530c21b946dc3b65960efebfec6a5e7c
Kevin Aspegren <kevin.aspegren@student.kyh.se>
2020-10-13 15:03:09 +0200

    Major update

--- src/com/company/YatziTest.java
+++ src/com/company/YatziTest.java
@@ -1,26 +1,31 @@
 package com.company;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static com.company.Dice.dice;
 
 public class YatziTest {
+
+    @DisplayName("Test if all dice have same value")
     @Test
-    void isYatziWhenAllDiceMatches() {
-        Die[] dice = new Die[5];
-        for(Die die: dice) {
-            die.value = 6;
+    void isYatzi() {
+        com.company.Dice.createDice();
+        for (com.company.Dice die : dice) {
+            die.value = 5;
         }
-        //Assert something?
+        assertTrue(com.company.BoardGameMaterial.checkIfYatzi(dice));
     }
 
+    @DisplayName("Test if all dice do not have same value")
     @Test
-    void isNotYatziWhenOneDieIsNotMatchingTheOther() {
-        Die[] dice = new Die[5];
-        for(Die die: dice) {
-            die.value = 6;
+    void isNotYatzi() {
+        com.company.Dice.createDice();
+        for (com.company.Dice die : dice) {
+            die.value = 5;
         }
-        dice[5].value = 1;
-        //Assert something?
+        dice[1].value = 1;
+        assertFalse(com.company.BoardGameMaterial.checkIfYatzi(dice));
     }
+
 }
